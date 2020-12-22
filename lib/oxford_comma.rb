2cd9e22470810b2f3a7b86dc2724e4a118a7cdc8
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-12-22 00:03:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  string = array.join
+  return string 
+end
 
+def oxford_comma(array)
+  string = array.join(" and ")
+  return string
+end
+
+def oxford_comma(array)
+  
 end
